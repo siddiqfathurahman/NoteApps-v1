d3871f014b1cbed924731f133741718f22f65da3
Muhammad Siddiq Fathurahman <129701382+siddiqfathurahman@users.noreply.github.com>
2025-03-21 23:07:43 +0700

    Update notesData.js

--- notesData.js
+++ notesData.js
@@ -5,13 +5,6 @@ export const notesData = [
       body: 'Welcome to Notes! This is your first note. You can archive it, delete it, or create new ones.',
       createdAt: '2022-07-28T10:03:12.594Z',
       archived: false,
-    },
-    {
-      id: 'notes-aB-cdefg12345',
-      title: 'Meeting Agenda',
-      body: 'Discuss project updates and assign tasks for the upcoming week.',
-      createdAt: '2022-08-05T15:30:00.000Z',
-      archived: false,
     }
   ];
   
