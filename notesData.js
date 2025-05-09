7c5893aeddcfc3571669889027538f9d2f87def4
Muhammad Siddiq Fathurahman <129701382+siddiqfathurahman@users.noreply.github.com>
2025-05-09 21:03:53 +0700

    Update notesData.js

--- notesData.js
+++ notesData.js
@@ -13,13 +13,7 @@ export const notesData = [
       createdAt: '2022-07-28T10:03:12.594Z',
       archived: false,
     },
-    {
-      id: 'notes-jT-jjsyz61J8XKiI',
-      title: 'Welcome to Notes, Dimas!',
-      body: 'Welcome to Notes! This is your first note. You can archive it, delete it, or create new ones.',
-      createdAt: '2022-07-28T10:03:12.594Z',
-      archived: false,
-    },
+
 
 
   ];
